a34207fe31441fba2f4677a8d27e8a8fb2e42dd3
Omorugie Oyibotha <66238145+RayRugie@users.noreply.github.com>
2023-06-02 21:19:41 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
+
+
+function App() {
+  return (
+    <div className="w-[1080px] m-auto">
+  <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
